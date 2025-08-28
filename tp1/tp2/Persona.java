e30b2100057c98ae1033e40fbcf7bcecf60efb14
Franco Herrera <fherrera@uarg.unpa.edu.ar>
2025-08-28 22:41:57 +0000

    update

---
+++ tp1/tp2/Persona.java
@@ -0,0 +1,42 @@
+package tp1.tp2;
+
+public class Persona {
+//Atributos
+  private String nombre;
+  private String apellido;
+  private int edad;
+  private int dni;
+//Constructor
+  public Persona(String nombre, String apellido, int edad, int dni)
+  {
+    this.nombre=nombre;
+    this.apellido=apellido;
+    this.edad=edad;
+    this.dni=dni;
+  }
+//metodos gets y sets
+  public String getNombre(){
+    return this.nombre;
+  }
+  public void setNombre(String nombre)
+  {
+    this.nombre=nombre;
+  }
+
+  public String getApellido(){
+    return this.apellido;
+  }
+  public void setApellido(String apellido)
+  {
+    this.apellido=apellido;
+  }
+
+  public boolean esMayorDeEdad()
+  {
+    if(edad >=18 )
+      return true;
+    else
+      return false;
+  }
+  
+}

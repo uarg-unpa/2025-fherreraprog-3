706b583eb11d7f363d7d9abf55b2551a49a9d0eb
Franco Herrera <fherrera@uarg.unpa.edu.ar>
2025-09-24 12:32:38 +0000

    update

---
+++ tp3/Historial.java
@@ -0,0 +1,27 @@
+package tp3;
+
+public class Historial {
+    private String[] paginas;
+    private int cima;
+    private final int MAX = 5;
+
+    public Historial() {
+        paginas = new String[MAX];
+        cima = -1;
+    }
+
+    public boolean estaVacia() { 
+        if (cima == -1)
+            return true;
+        return false;
+     }
+    public boolean estaLlena() { return cima == MAX - 1; }
+    public void meter(String pagina) {
+        cima++;    
+        paginas[cima] = pagina;
+    }
+    public String sacar() {
+            return paginas[cima--];
+    }
+}
+  
